0e6701055e673136b78113632495a4e8b4ca7887
ReliableRoseAffairs <164271757+ReliableRoseAffairs@users.noreply.github.com>
2024-06-01 01:46:19 +0100

    Add files via upload

---
+++ Button.js
@@ -0,0 +1,33 @@
+
+function redirectToPage() {
+  // Redirect to the desired page
+  window.location.href = "self-managed.html";
+}
+function redirectTopersonal() {
+  // Redirect to the desired page
+  window.location.href = "personal-advisor.html";
+}
+function redirectToadvisorservice() {
+  // Redirect to the desired page
+  window.location.href = "wealth.html";
+}
+function redirectToselfmanage() {
+  // Redirect to the desired page
+  window.location.href = "self-managed.html";
+}
+function redirectToadvisors() {
+  // Redirect to the desired page
+  window.location.href = "wealth.html";
+}
+function redirectTocreate() {
+  // Redirect to the desired page
+  window.location.href = "create-account.html";
+}
+function redirectToprivate() {
+  // Redirect to the desired page
+  window.location.href = "interest.html";
+}
+function redirectTowealthone() {
+  // Redirect to the desired page
+  window.location.href = "wealth-strategy-one.html";
+}
